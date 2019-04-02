2ef687ba31b6353296e5c74c38c6af9414f11462
Conor Bury <conordebru@gmail.com>
2019-04-02 10:29:38 +0100

    Fixed Search Through Database Function

--- app/src/main/java/com/example/recyclelabtech/Tab2Fragment.java
+++ app/src/main/java/com/example/recyclelabtech/Tab2Fragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,32 +15,41 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
 
 public class Tab2Fragment extends Fragment {
-    EditText mSearchField;
-    ImageButton mSearchBtn;
-    RecyclerView mResultList;
+
     DatabaseReference mProductDatabase;
+    RecyclerView mResultList;
     View myView;
+    ImageButton mSearchBtn;
+    EditText mSearchField;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.fragment_two, container, false);
 
+        mSearchBtn = (ImageButton) myView.findViewById(R.id.search_btn);
         mProductDatabase = FirebaseDatabase.getInstance().getReference("Items");
-        mSearchField = (EditText) getActivity().findViewById(R.id.search_field);
-        mSearchBtn = (ImageButton) getActivity().findViewById(R.id.search_btn);
+        mSearchField =  myView.findViewById(R.id.search_field);
+        mResultList = myView.findViewById(R.id.result_list);
 
-        mResultList = (RecyclerView) getActivity().findViewById(R.id.result_list);
-        //mResultList.hasFixedSize();
-        //mResultList.setLayoutManager(new LinearLayoutManager(getActivity()));
-        //mResultList.setLayoutManager(new LinearLayoutManager(getContext()));
+        mResultList.setHasFixedSize(true);
+        mResultList.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-
+        mSearchField.setOnKeyListener(new View.OnKeyListener(){
+            public boolean onKey(View v, int keyCode, KeyEvent event){
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    String searchText = mSearchField.getText().toString();
+                    firebaseProductSearch(searchText);
+                    return true;
+                }
+                return false;
+            }
+        });
         mSearchBtn.setOnClickListener(new View.OnClickListener(){
            @Override
            public void onClick(View view){
@@ -52,32 +62,22 @@ public class Tab2Fragment extends Fragment {
     }//End onCreateView method
 
     private void firebaseProductSearch(String searchText) {
-        Query firebaseSearchQuery = mProductDatabase.orderByChild("name").startAt(searchText).endAt(searchText + "\uf8ff");
-        FirebaseRecyclerOptions<products> options =
-                new FirebaseRecyclerOptions.Builder<products>()
-                .setQuery(firebaseSearchQuery
-
-                        , products.class)
-                .build();
-        FirebaseRecyclerAdapter adapter = new FirebaseRecyclerAdapter<products, ProductViewHolder>(options){
-            @Override
-            public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.list_layout, parent, true);
-
-                return new ProductViewHolder(view);
-            }
-
-            @Override
-            public void onBindViewHolder(ProductViewHolder holder, int position, products model) {
-                holder.setDetails(getActivity().getApplicationContext(), model.getName(), model.getDescription(), model.getBarcodefmt(), model.getManufactor(), model.getImage());
+        Query firebaseSearchQuery = mProductDatabase.orderByChild("name").startAt(searchText).endAt(searchText+"\uf8ff");
+        FirebaseRecyclerAdapter<products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<products, ProductViewHolder>(
+                products.class,
+                R.layout.list_layout,
+                ProductViewHolder.class,
+                firebaseSearchQuery
+        ){
+            protected void populateViewHolder(ProductViewHolder holder, products model, int position) {
+                holder.setDetails(myView.getContext(), model.getName(), model.getDescription(), model.getBarcodefmt(), model.getManufactor(), model.getImage());
             }
         };
 
         mResultList.setAdapter(adapter);
     }
 
-    public static class ProductViewHolder extends RecyclerView.ViewHolder{
+    public static class ProductViewHolder extends RecyclerView.ViewHolder {
 
         View mView;
 
@@ -86,12 +86,12 @@ public class Tab2Fragment extends Fragment {
             mView = itemView;
         }//End constructor
 
-        public void setDetails(Context ctx, String name, String description, String manufator, String barcodefmt, String image){
-            TextView product_name = (TextView) mView.findViewById(R.id.name_text);
-            TextView product_description = (TextView) mView.findViewById(R.id.description_text);
-            TextView product_manufactor = (TextView) mView.findViewById(R.id.manufactor_text);
-            TextView product_barcode = (TextView) mView.findViewById(R.id.barcode_text);
-            ImageView product_image = (ImageView) mView.findViewById(R.id.product_image);
+        public void setDetails(Context ctx, String name, String description, String manufator, String barcodefmt, String image) {
+            TextView product_name = mView.findViewById(R.id.name_text);
+            TextView product_description = mView.findViewById(R.id.description_text);
+            TextView product_manufactor = mView.findViewById(R.id.manufactor_text);
+            TextView product_barcode = mView.findViewById(R.id.barcode_text);
+            ImageView product_image = mView.findViewById(R.id.product_image);
 
             product_name.setText(name);
             product_description.setText(description);
@@ -100,7 +100,6 @@ public class Tab2Fragment extends Fragment {
 
             Glide.with(ctx).load(image).into(product_image);
         }
+    }
 
-
-    }//End class
 }//End class
